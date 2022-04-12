486a3d11d4d77186927d1ed7978683c66c3703a9
map <1david.vdoviak>
2022-04-12 13:10:18 +0200

    Initial commit

---
+++ src/ZLOMOK/Main.java
@@ -0,0 +1,18 @@
+package ZLOMOK;
+//názov súboru a názov hlavnej triedy musia byť rovnaké(Main.java , public class Main)
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+
+Trieda ZLOMOK{}
+Zlomok("3/4")//výnimky
+index of(/)
+matica trojuholníkova (stringsplit()- od určitého znaku rozdeľuje string)
+zlomky: +;-;*;/;
+Prevod na cele čislo
+ */
---
+++ src/ZLOMOK/Zlomok.java
@@ -0,0 +1,24 @@
+package ZLOMOK;
+
+public class Zlomok {
+    private int m;
+    private int n;
+
+    /*
+
+Trieda ZLOMOK{}
+Zlomok("3/4")//výnimky
+index of(/)
+matica trojuholníkova
+zlomky: +;-;*;/;
+Prevod na cele čislo
+ */
+
+    public boolean dajZlomok(String zlomok)
+    {
+        //substring()- berie od určitého bodu do určítého bodu
+
+    }
+
+
+}
