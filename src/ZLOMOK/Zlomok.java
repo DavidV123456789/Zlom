e9578814b77347583066c01babb078bfe3822143
map <1david.vdoviak>
2022-04-20 08:44:28 +0200

    l

--- src/ZLOMOK/Zlomok.java
+++ src/ZLOMOK/Zlomok.java
@@ -1,7 +1,6 @@
 package ZLOMOK;
 
 
-
 public class Zlomok {
     private int m;
     private int n;
@@ -16,39 +15,128 @@ zlomky: +;-;*;/;
 Prevod na cele čislo
  */
 
+
     public boolean dajZlomok(String zlomok) {
         //substring()- berie od určitého bodu do určítého bodu
         //strchr//hlada znak v retazci
         //strstr//hlada retazec v retazci
         //atd
+
         int d = strlen(zlomok);
         int i;
         int z = 0;
+        int zz=0;
         for (i = 0; i < d; i++) {
             if (zlomok[i] == '/') {
                 z = i;
+                zz++;
             }
         }
-
-        for (i = z; i >= 0; i--) {
-            int c;
-            int d=
-            switch(zlomok[i])
-            {
-                case"1":c=1;
-                case"2":c=2;
-                case"3":c=3;
-                case"4":c=4;
-                case"5":c=5;
-                case"6":c=6;
-                case"7":c=7;
-                case"8":c=8;
-                case"9":c=9;
-                default:/*vynimka(throw)*/;
+        try {
+            if (z == 1) {
+                int decal = 1;
+                for (i = z; i >= 0; i--) {
+                    int c;
+                    decal *= 10;
+                    switch (zlomok[i]) {
+                        case "1":
+                            c = 1;
+                        case "2":
+                            c = 2;
+                        case "3":
+                            c = 3;
+                        case "4":
+                            c = 4;
+                        case "5":
+                            c = 5;
+                        case "6":
+                            c = 6;
+                        case "7":
+                            c = 7;
+                        case "8":
+                            c = 8;
+                        case "9":
+                            c = 9;
+                        default:
+                            c = 0;throw new Exception_n;//urob tiedy
+                    }
+                    n = c * decal;
+                }
+                decal = 1;
+                for (i = d - 1; i >= z; i--) {
+                    int c;
+                    decal *= 10;
+                    switch (zlomok[i]) {
+                        case "1":
+                            c = 1;
+                        case "2":
+                            c = 2;
+                        case "3":
+                            c = 3;
+                        case "4":
+                            c = 4;
+                        case "5":
+                            c = 5;
+                        case "6":
+                            c = 6;
+                        case "7":
+                            c = 7;
+                        case "8":
+                            c = 8;
+                        case "9":
+                            c = 9;
+                        default:
+                            c = 0;throw new Exception_m;//urob tiedy ;
+                    }
+                    m = c * decal;
+                }
+                //...
+            } else if (z == 0) {
+                int decal = 1;
+                for (i = d - 1; i >= 0; i--) {
+                    int c;
+                    decal *= 10;
+                    switch (zlomok[i]) {
+                        case "1":
+                            c = 1;
+                        case "2":
+                            c = 2;
+                        case "3":
+                            c = 3;
+                        case "4":
+                            c = 4;
+                        case "5":
+                            c = 5;
+                        case "6":
+                            c = 6;
+                        case "7":
+                            c = 7;
+                        case "8":
+                            c = 8;
+                        case "9":
+                            c = 9;
+                        default:
+                            c = 0;throw new Exception_n;//urob tiedy ;
+                    }
+                    n = c * decal;
+                    m = 1;
+                }
+            } else {
+                throw new Exception_lomeno;//urob tiedy
             }
-            n=c*d;
         }
-
+        catch (Exception_n ex)
+        {
+            System.out.println("");
+        }
+        catch (Exception_m ex)
+        {
+            System.out.println("");
+        }
+        catch (Exception_lomeno ex)
+        {
+            System.out.println("");
+        }
     }
 
 
